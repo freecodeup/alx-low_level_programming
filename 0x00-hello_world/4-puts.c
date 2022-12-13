80816168e20e06d70f27faa9aaa616575857997c
oluchi anyachor <oluchipep@gmail.com>
2022-12-13 04:32:15 -0800

    hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
- *main - Entry Point
- *Return: Always 0 (success)
+ * main - Entry point
+ * Retrun: Always 0 (success)
 */
 
 int main(void)
 {
-	puts("programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
